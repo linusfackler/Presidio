b36ea5e2e260e1ea4d728867c3812a00e4b6a585
Linus Fackler <78768883+linusfackler@users.noreply.github.com>
2023-07-11 01:44:01 +0200

    update

--- presidio-chat-bot/src/App.js
+++ presidio-chat-bot/src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from 'react';
+import axios from 'axios';
+
 
 function App() {
+  const [userMessage, setUserMessage] = useState('');
+  const [chatHistory, setChatHistory] = useState([]);
+  const chatContainerRef = useRef(null);
+
+  const handleMessageChange = (event) => {
+    setUserMessage(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post('/chat', { message: userMessage });
+
+      // Add user message and chatbot reply to the chat history
+      const newChatEntry = {
+        userMessage,
+        botMessage: response.data.reply,
+      };
+
+      setChatHistory((prevChatHistory) => [...prevChatHistory, newChatEntry]);
+    } catch (error) {
+      console.error(error);
+    }
+
+    setUserMessage('');
+  };
+
+  useEffect(() => {
+    // Scroll to the bottom of the chat container after chat history updates
+    chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
+  }, [chatHistory]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ChatGPT from wish.com</h1>
+      <div ref={chatContainerRef} className="ChatContainer">
+        {chatHistory.map((chatEntry, index) => (
+          <div key={index} className="ChatEntry">
+            <p>User: {chatEntry.userMessage}</p>
+            <p>Bot: {chatEntry.botMessage}</p>
+          </div>
+        ))}
+      </div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Type your message"
+          value={userMessage}
+          onChange={handleMessageChange}
+        />
+        <button type="submit">Send</button>
+      </form>
     </div>
   );
 }
---
+++ presidio-chat-bot/src/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const { Configuration, OpenAIApi } = require("openai");
+require('dotenv').config()
+
+const app = express();
+const port = 3001;
+
+// Set up your OpenAI API credentials
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+// Enable JSON body parsing
+app.use(express.json());
+
+// Handle root GET request
+app.get('/', (req, res) => {
+  res.send('Welcome to the chatbot backend!');
+});
+
+app.post('/chat', async (req, res) => {
+  const { message } = req.body;
+
+  // Make a request to the OpenAI API
+  const response = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      { role: 'system', content: 'You are a helpful assistant.' },
+      { role: 'user', content: message },
+    ],
+  });
+
+  const reply = response.data.choices[0].message.content;
+
+  res.json({ reply });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
