80c70716404ccdf86b52465e078a0625768f9e09
Linus Fackler <78768883+linusfackler@users.noreply.github.com>
2023-07-11 07:41:26 +0200

    update

--- presidio-chat-bot/src/server.js
+++ presidio-chat-bot/src/server.js
@@ -40,6 +40,7 @@ app.post('/chat', async (req, res) => {
   res.json({ reply });
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(process.env.PORT || 3001, () => {
+  console.log(`Server running on port ${process.env.PORT || 3001}`);
 });
+
