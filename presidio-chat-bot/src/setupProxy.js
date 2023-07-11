1748eb658bb2b6a065c99c89a5304d017e7d6721
Linus Fackler <78768883+linusfackler@users.noreply.github.com>
2023-07-11 05:07:07 +0200

    update

--- presidio-chat-bot/src/setupProxy.js
+++ presidio-chat-bot/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     '/chat',
     createProxyMiddleware({
-      target: 'http://localhost:3001',
+      target: process.env.REACT_APP_API_URL,
       changeOrigin: true,
     })
   );
