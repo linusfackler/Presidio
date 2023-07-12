f910a6409303fa855227a619b27dee4b84f33c9d
Linus Fackler <78768883+linusfackler@users.noreply.github.com>
2023-07-12 18:53:07 +0200

    update

---
+++ backend/server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const cors = require('cors');
+const { Configuration, OpenAIApi } = require("openai");
+require('dotenv').config()
+const app = express();
+const port = 3001;
+
+// Set up your OpenAI API credentials
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const corsOptions = {
+  origin: 'https://linusfackler.github.io',
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+// app.use(cors())
+
+// Enable JSON body parsing
+app.use(express.json());
+
+// Handle root GET request
+app.get('/', (req, res) => {
+  res.send('Welcome to the chatbot backend!');
+});
+
+app.post('/chat', async (req, res) => {
+  const { message } = req.body;
+
+  // Make a request to the OpenAI API
+  const response = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      { role: 'system', content: 'You are a helpful assistant.' },
+      { role: 'user', content: message },
+    ],
+  });
+
+  const reply = response.data.choices[0].message.content;
+
+  res.json({ reply });
+});
+
+app.listen(process.env.PORT || port, () => {
+  console.log(`Server running on port ${process.env.PORT || port}`);
+});
+
---
+++ frontend/src/setupProxy.js
@@ -0,0 +1,12 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function (app) {
+  app.use(
+    '/chat',
+    createProxyMiddleware({
+      target: 'https://presidio.azurewebsites.net',
+      // target: 'http://localhost:3001',
+      changeOrigin: true,
+    })
+  );
+};
