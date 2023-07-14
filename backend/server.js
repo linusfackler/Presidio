eb3279304ee645e0832ea382053762bd4a7140eb
Linus Fackler <78768883+linusfackler@users.noreply.github.com>
2023-07-14 16:10:47 +0200

    update

--- backend/server.js
+++ backend/server.js
@@ -12,19 +12,28 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const corsOptions = {
-  origin: 'https://linusfackler.github.io',
+  origin: function (origin, callback) {
+    const whitelist = ['https://linusfackler.github.io', 'http://localhost:3000'];
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
 };
 
 app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://linusfackler.github.io');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  next();
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.send();
+  } else {
+    next();
+  }
 });
+
 
 // Enable JSON body parsing
 app.use(express.json());
